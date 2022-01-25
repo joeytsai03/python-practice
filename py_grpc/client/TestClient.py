42fe594cd424855f3931dba0cd1d34b58314f3cb
caizhy <c494003007@qq.com>
2022-01-25 18:24:35 +0800

    fix

--- py_grpc/client/TestClient.py
+++ py_grpc/client/TestClient.py
@@ -25,20 +25,3 @@ def run():
 
 if __name__ == '__main__':
     run()
-
-
-def run():
-    '''
-    模拟请求服务方法信息
-    :return:
-    '''
-    conn = grpc.insecure_channel('localhost:50052')
-    client = hello_pb2_grpc.GrpcServiceStub(channel=conn)
-    skill = hello_pb2.Skill(name="engineer")
-    request = hello_pb2.HelloRequest(data="xiao gang", skill=skill)
-    response = client.hello(request)
-    print("received:", response.result)
-
-
-if __name__ == '__main__':
-    run()
